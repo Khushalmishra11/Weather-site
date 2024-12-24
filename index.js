89a83c02eda906a905767549a117b67d95d43925
Khushal Mishra <khushalmishra261203@gmail.com>
2024-12-24 22:51:06 +0530

    basic website created

--- index.js
+++ index.js
@@ -7,14 +7,44 @@ const searchBtn = document.querySelector(".search button");
 
 async function checkWeather(city) {
   const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-  var data = await response.json();
-  console.log(data);
-  document.querySelector(".city").innerHTML = data.name;
-  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
-  document.querySelector(".wind").innerHTML = data.wind.speed + "Km/h";
-  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-  document.querySelector(".weather").style.display = "block"
-  const icon = d
+
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    var data = await response.json();
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + "°C";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "Km/h";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    const icon = data.weather[0].main;
+    switch (icon) {
+      case "Clouds":
+        document.querySelector(".weather-icon").src = "assest/clouds.png";
+        break;
+      case "Rain":
+        document.querySelector(".weather-icon").src = "assest/rain.png";
+        break;
+      case "Clear":
+        document.querySelector(".weather-icon").src = "assest/clear.png";
+        break;
+      case "Snow":
+        document.querySelector(".weather-icon").src = "assest/snow.png";
+        break;
+      case "Drizzle":
+        document.querySelector(".weather-icon").src = "assest/drizzle.png";
+        break;
+      case "Mist":
+        document.querySelector(".weather-icon").src = "assest/mist.png";
+        break;
+      default:
+        document.querySelector(".weather-icon").src = "assest/clouds.png";
+    }
+
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+  }
 }
 
 searchBtn.addEventListener("click", () => {
